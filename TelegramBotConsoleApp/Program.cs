eaa7c3f1ca22b0d438b9248704cac987ee33b92c
Rashad133 <rashadrufullayev8@gmail.com>
2024-09-05 18:19:34 +0400

    salam

---
+++ TelegramBotConsoleApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace TelegramBotConsoleApp
+{
+	internal class Program
+	{
+		static TelegramBotClient Bot = new TelegramBotClient("7490122706:AAF0Tv-9y_gKlIVDH7rpOYjykcMqc31WxI8");
+		static HttpClient httpClient = new HttpClient();
+		static async Task Main(string[] args)
+		{
+
+			using var cts = new CancellationTokenSource();
+
+			Bot.StartReceiving(
+				HandleUpdate,
+				HandleErrorAsync,
+				new ReceiverOptions { AllowedUpdates = Array.Empty<UpdateType>() },
+				cancellationToken: cts.Token
+				);
+			Console.WriteLine("Bot");
+			await Task.Delay(-1);
+		}
+
+		public static async Task HandleUpdate(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+		{
+			if (update.Type == UpdateType.Message && update.Message!.Type == MessageType.Text)
+			{
+				var chatId = update.Message.Chat.Id;
+				var messageText = update.Message.Text;
+
+				Console.WriteLine($"Sending message text for User {update.Message.Chat.Username}:{messageText}");
+
+				await botClient.SendTextMessageAsync(
+					chatId: chatId,
+					text: "Salam aleykum " + update.Message.Chat.Username,
+					cancellationToken: cancellationToken
+				);
+			}
+			if (update.Type == UpdateType.Message && update.Message!.Type == MessageType.Audio)
+			{
+				var chatId = update.Message.Chat.Id;
+				var messageAudio = update.Message.Audio;
+
+				Console.WriteLine($"Sending audio message for User {update.Message.Chat.Username}:{messageAudio}");
+
+				var audioUrl = "https://telegrambots.github.io/book/docs/audio-guitar.mp3";
+				var fileStream = await httpClient.GetStreamAsync(audioUrl);
+
+				//await botClient.SendAudioAsync(
+				//	chatId: chatId,
+				//	audio: new InputFile(fileStream,"audio-guitar.mp3"),
+				//	cancellationToken: cancellationToken
+				//);
+			}
+		}
+		public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+		{
+			Console.WriteLine($"Error: {exception.Message}");
+			return Task.CompletedTask;
+		}
+
+
+	}
+}
